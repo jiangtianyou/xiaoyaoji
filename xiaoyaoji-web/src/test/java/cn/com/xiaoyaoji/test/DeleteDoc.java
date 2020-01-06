0b8fb86b286281db732db8fb0ef877d9d23fc741
Stypstive <420671626@qq.com>
2020-01-06 09:47:43 +0800

    delete comment

--- xiaoyaoji-web/src/test/java/cn/com/xiaoyaoji/test/DeleteDoc.java
+++ xiaoyaoji-web/src/test/java/cn/com/xiaoyaoji/test/DeleteDoc.java
@@ -4,12 +4,6 @@ import cn.com.xiaoyaoji.data.DataFactory;
 import cn.com.xiaoyaoji.service.ServiceFactory;
 import org.junit.Test;
 
-/**
- * ...
- *
- * @author : liangfen.zhou@huolala.cn
- * Date : 2019-04-17 17:33
- */
 public class DeleteDoc {
 
     @Test
